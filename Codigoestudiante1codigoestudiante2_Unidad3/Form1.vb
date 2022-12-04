97059669ffebb0e039655e47031441601e7b3f28
carso <carso@DESKTOP-VJUHKNA>
2022-12-03 20:11:18 -0600

    Agregar archivos de proyecto.

---
+++ Codigoestudiante1codigoestudiante2_Unidad3/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Dim alumno As New Alumno
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+
+        alumno.datosAlumno(txtCodigo.Text, txtNombre.Text, txtApellido.Text, txtSexo.Text, txtDireccion.Text)
+
+        If alumno.datosAceptados Then
+            MsgBox("Alumno" & alumno.nombreAlumno & "Registrado")
+        Else
+
+        End If
+
+    End Sub
+
+    Private Sub btnGenerar_Click(sender As Object, e As EventArgs) Handles btnGenerar.Click
+        If txtNombre.Text = "" Then
+            MsgBox("Debe ingresar su nombre")
+
+        Else
+            txtCodigo.Text = alumno.generarCodigo(txtNombre.Text)
+        End If
+    End Sub
+End Class
